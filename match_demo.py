df6a08808f0fda30348f364db8fde026b22ff97d
Ben Guidarelli <ben.guidarelli@gmail.com>
2022-12-09 13:40:07 -0500

    adding match demo

--- match_demo.py
+++ match_demo.py
@@ -16,7 +16,7 @@ from beaker.sandbox import get_accounts, get_algod_client
 
 def compile_program(algod: AlgodClient) -> bytes:
     # read in file
-    with open("framepointer.teal", "r") as f:
+    with open("match.teal", "r") as f:
         teal_src = f.read()
 
     compile_result = algod.compile(teal_src)
@@ -54,17 +54,18 @@ def demo():
     # save the app id for the app we just created
     app_id = result["application-index"]
 
+
     # Define the method we want to call
-    pythag_method = Method.from_signature("pythag(uint64,uint64)uint64")
+    add_method = Method.from_signature("add(uint8,uint8)uint16")
 
     # create ATC to call the method, passing 3,4 as args
     atc = AtomicTransactionComposer()
-    atc.add_method_call(app_id, pythag_method, acct.address, sp, acct.signer, [3, 4])
+    atc.add_method_call(app_id, add_method, acct.address, sp, acct.signer, [3, 4])
     results: AtomicTransactionResponse = atc.execute(algod, 4)
     print(f"Confirmed in round {results.confirmed_round}")
 
-    # Make sure we got what we expect (3**2 + 4**2 == 5**2)
-    assert results.abi_results[0].return_value == 5
+    # Make sure we got what we expect (3+4 == 7)
+    assert results.abi_results[0].return_value == 7
 
 
 if __name__ == "__main__":
